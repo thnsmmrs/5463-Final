769ceddcd94271577b1a9bf4fe183a06427028cd
thnsmmrs <53453187+thnsmmrs@users.noreply.github.com>
2025-12-09 15:07:20 -0500

    Update RRT.py

--- RRT.py
+++ RRT.py
@@ -1,4 +1,32 @@
-def rrt(frame, start, end, steps):
-    path = 1
+##From Github.com/nimRobotics/RRT
+
+import cv2
+import numpy as np
+import math
+import random
+import argparse
+import os
+
+class Nodes: #to store created graph
+    def init(self, x,y):
+        self.x = x
+        self.y = y
+        self.parent_x = []
+        self.parent_y = []
     
+node_list = []
+debug = True #change to false for final
+
+def rrt(frame, start, end, steps):
+    h,w = frame.shape #capturing dims for coordinate system in this case 1280x720
+    node_list[0] = Nodes(start[0],start[1])
+    node_list[0].parent_x.append(start[0])
+    node_list[0].parent_y.append(start[1])
+    
+    if debug == True:
+        cv2.circle(frame, (start[0],start[1]), 5,(0,0,255),thickness=3, lineType=8)
+        cv2.circle(frame, (end[0],end[1]), 5,(0,0,255),thickness=3, lineType=8)
+        path = "True"
+    path = "Fail"
+
     return path
