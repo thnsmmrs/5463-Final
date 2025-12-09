bb8220e979b86a2ca08cda65dfa7ac4f5ca26f2b
thnsmmrs <53453187+thnsmmrs@users.noreply.github.com>
2025-12-09 16:08:49 -0500

    Refactor obstacle detection and improve mask processing

--- ObstacleDetection.py
+++ ObstacleDetection.py
@@ -15,15 +15,10 @@ def mouse_callback(event, x, y, flags, param):
 cv2.namedWindow("Overlay")
 cv2.setMouseCallback("Overlay", mouse_callback)
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,  1280)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,  720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 #Locks resolution at 720p (from logi c270 - stays same for all cameras)
-ret,background = cap.read()
-#grabs first frame and initilizes the background
-background = cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
-background = cv2.GaussianBlur(background,(5,5),0)
-#will be used to filter background
-h,w = background.shape[:2]
+
 start = (50,50)
 #define home position (can be in terms of h,w for ratios)
 
@@ -36,20 +31,17 @@ while True:
     #Reads next frames continously
     if not ret:
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    #Converts frame to grayscale and blurs it to reduce noise and improve detection accuracy
-    diff = cv2.absdiff(background, gray)
-    #Finds the difference between background and current frame
+    blur = cv2.GaussianBlur(frame, (5,5), 0)
+    color_hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    white = cv2.inRange(color_hsv, (0,0,50), (180,40,255))
+    fg_mask = cv2.bitwise_not(white)
 
-    _, fg_mask = cv2.threshold(diff,Threshold,255,cv2.THRESH_BINARY)
-
-    contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    fg_mask = improve_obstacle_mask(fg_mask, 100)
+    
+    #fg_mask = improve_obstacle_mask(fg_mask, 100)
     #implementing improve obstacle mask function to clean up mask using morphology
-    obstacle_mask = np.zeros_like(fg_mask)
-    for valid_contours in contours:
-        cv2.drawContours(obstacle_mask, [valid_contours], -1, (255), thickness=-1)
+    obstacle_mask = fg_mask.copy()
+    obstacle_mask = improve_obstacle_mask(obstacle_mask)
+    #obstacle_mask = cv2.bitwise_not(fg_mask)
     overlay = frame.copy()
     overlay[obstacle_mask == 255] = (255, 255, 255)  # Highlight obstacles in red (can change color)
     vis = cv2.addWeighted(frame, overlayStrength, overlay, 1 - overlayStrength, 0)
@@ -62,7 +54,7 @@ while True:
         print(path)
     cv2.imshow("Overlay", vis)
     #print(path) #debug
-    #cv2.imshow("Obstacle Mask", obstacle_mask) #improved qual
+    cv2.imshow("Obstacle Mask", obstacle_mask) #improved qual
 
     
     if cv2.waitKey(1) & 0xFF == 27: #ESC to end program
