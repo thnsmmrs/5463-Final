886b02b57376c7a1c6ce33df4277e211884e0b9c
krishm90210 <krishm90210@gmail.com>
2025-12-10 15:18:07 -0500

    Update robot_kinematics.py

--- robot_kinematics.py
+++ robot_kinematics.py
@@ -33,25 +33,35 @@ class Robot3R:
         x3 = x2 + self.L3 * np.cos(theta1 + theta2 + theta3)
         y3 = y2 + self.L3 * np.sin(theta1 + theta2 + theta3)
 
-        return [(x0,y0), (x1,y1), (x2,y2), (x3,y3)]
-    # IK
-    def inverse_kinematics(self, target_x, target_y):
+        return [(x0, y0), (x1, y1), (x2, y2), (x3, y3)]
+    # IK, used Lectures 11 and 12 from class notes as reference for equation derivations
+    def inverse_kinematics(self, target_x, target_y, elbow = 'righty'):
         dx = target_x - self.base_x
         dy = target_y - self.base_y
+        # Distance to target
         d = np.sqrt(dx**2 + dy**2)
+        # Angle to target
+        gamma = np.arctan2(dy, dx)
         # 2-link arm (L2 + L3)
         L2_3 = self.L2 + self.L3
         # Checking reachability of joint 2 (wrist)
-        if d > self.L1 + L2_3:
-            return None
-        # Solving for theta2
-        cos_theta2 = (self.L1**2 + L2_3**2 - d**2) / (2 * self.L1 * L2_3)
-        cos_theta2 = np.clip(cos_theta2, -1.0, 1.0)
-        theta2 = np.arccos(cos_theta2)
-        # Solving for theta1
-        k1 = self.L1 + L2_3 * np.cos(theta2)
-        k2 = L2_3 * np.sin(theta2)
-        theta1 = np.arctan2(dy, dx) - np.arctan2(k2, k1)
+        if d > (self.L1 + L2_3) or d < abs(self.L1 - L2_3):
+            return None # unreachable target
+        # Solving for alpha using Law of Cosines (angle opposite d)
+        cos_alpha = (self.L1**2 + d**2 - L2_3**2) / (2 * self.L1 * d)
+        cos_alpha = np.clip(cos_alpha, -1.0, 1.0)
+        alpha = np.arccos(cos_alpha)
+        # Solving for beta using Law of Cosines (angle at base)
+        cos_beta = (self.L1**2 + L2_3**2 - d**2) / (2 * self.L1 * L2_3)
+        cos_beta = np.clip(cos_beta, -1, 1)
+        beta = np.arccos(cos_beta)
+        # Loop to check if elbow-up or elbow-down
+        if elbow == 'righty':
+            theta1 = gamma - alpha
+            theta2 = np.pi - beta
+        else: # lefty
+            theta1 = gamma + alpha
+            theta2 = beta - np.pi
         # Joint 2 location
         x1 = self.base_x + self.L1 * np.cos(theta1)
         y1 = self.base_y + self.L1 * np.sin(theta1)
